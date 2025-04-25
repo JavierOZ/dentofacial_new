a99ad59e39158d9a40993acfa5e73f25baa0828d
Javier Gonzalez <javiergonzalez@MacBook-Pro-de-Javier.local>
2025-04-25 13:46:00 -0400

    cambio home v2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 import App from './App'; // Página principal
 import Admin from './Admin'; // Página de administración
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+      <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/admin" element={<Admin />} />
+       <Route path="/admin" element={<Admin />} />
+      </Routes>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
